42826339293c2219aa3a9a4884eec7a1c29b26bd
김예진 <a89557400@gmail.com>
2023-02-03 19:27:40 +0900

    Feat: 스케줄러 작성

---
+++ src/main/java/com/hanbange/hanbange_batch/scheduler/ReceivingSchesuler.java
@@ -0,0 +1,39 @@
+package com.hanbange.hanbange_batch.scheduler;
+
+import java.util.Collections;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class ReceivingSchesuler {
+
+	private final Job ReceivingJob;
+	private final JobLauncher jobLauncher;
+
+	@Scheduled(cron = "0 */1 * * * *")
+	public void ReceivingJobRun() throws
+		JobInstanceAlreadyCompleteException,
+		JobExecutionAlreadyRunningException,
+		JobParametersInvalidException,
+		JobRestartException {
+
+		JobParameters jobParameters = new JobParameters(
+			Collections.singletonMap("requestTime", new JobParameter(System.currentTimeMillis()))
+		);
+
+		jobLauncher.run(ReceivingJob, jobParameters);
+	}
+
+}
